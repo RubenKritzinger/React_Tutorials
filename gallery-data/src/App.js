9d82e131488f0ef86ca6d05423bc78ccc6d3453d
RubenKritzinger <rubenkritzinger1998@gmail.com>
2022-06-14 13:22:46 +0200

    Gallery-data

---
+++ gallery-data/src/App.js
@@ -0,0 +1,9 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./gallery/Gallery";
+function App() {
+ // render(){
+  return (
+    <Gallery />
+  );
+  }
+export default App;
